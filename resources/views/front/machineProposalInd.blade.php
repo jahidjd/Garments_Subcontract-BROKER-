cf8e18e2e15b471f682d24cfddc92d6668d49618
jahidjd <jahidjd18@gmail.com>
2022-08-17 15:30:21 +0600

    Broker

---
+++ resources/views/front/machineProposalInd.blade.php
@@ -0,0 +1,75 @@
+@extends('front.layout')
+@section('content')
+<!-- ======================= Page Title ===================== -->
+<div class="page-title">
+    <div class="container">
+        <div class="page-caption">
+            <h2>Proposal List</h2>
+            <p><a href="{{route('dashboard')}}" title="User Dashboard">User Dashboard</a> <i class="ti-angle-double-right"></i> Proposal List</p>
+        </div>
+    </div>
+</div>
+<!-- ======================= End Page Title ===================== -->
+
+<!-- ================ Profile Settings ======================= -->
+<section class="padd-top-80 padd-bot-80">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div id="leftcol_item">
+                    <div class="user_dashboard_pic"> <img alt="user photo"
+                            src="{{asset('photos/profile/'.$data->photo)}}"> <span class="user-photo-action"> {{
+                            Auth::user()->name }}</span> </div>
+                </div>
+                @include('front.dashSidebar')
+            </div>
+            <div class="col-md-9">
+            <a href="{{route('dashboard')}}" class="btn btn-md " style="width:12%;background:#548CA8;color:white;"><i class="fas fa-arrow-left" style="color:white"></i>&nbsp;&nbsp;Back</a>
+            <h3></h3>
+                <div class="profile_detail_block">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>SL</th>
+                                <th>Title</th>
+                                <th>Company Name</th>
+                                <th>Budget</th>
+                                <th>Deadline</th>
+                                <th>Status</th>
+                              
+                                <th>#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse  ($proposal as $key=>$item)
+                            <tr>
+                                <td>{{++$key}}</td>
+                                <td>{{$item->title}}</td>
+                                <td>{{$item->workorder->Companies->name}}</td>
+                                {{-- <td>{{$item->machinePost->usermachines->company->name}}</td> --}}
+                                <td>{{$item->budget}}</td>
+                                <td>{{$item->deadline}}</td>
+                                <td>{{$item->status}}</td>
+                               
+                                <td>
+                                    
+                                    
+                                    
+                                    <a href="{{route('proposal_details',$item->id)}}" class="btn btn-success btn-sm"><i
+                                            class="fa-solid fa-bars"></i></a>
+
+                                </td>
+                            </tr>
+                            @empty
+                              <h1>No Proposal Found</h1>
+
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ================ End Profile Settings ======================= -->
+@endsection
